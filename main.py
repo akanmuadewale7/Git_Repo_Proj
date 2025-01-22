7a2edbe8a8b0a9f4f243666a41e2dd9a9ae898cf
Adewale-Akanmu <akanmuadewale7@gmail.com>
2025-01-22 12:49:40 +0100

    modified the API script

---
+++ main.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from datetime import datetime
+app = Flask(__name__)
+ 
+@app.route('/hello/<name>')
+def hello_name(name):
+   return 'Hello %s!' % name
+
+
+@app.route('/bye/<name>')
+def bye_name(name):
+   return 'Bye %s!' % name
